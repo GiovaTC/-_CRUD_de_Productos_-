77f84ae92f1f4b53007384780e9b3f438bfce0da
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-01 16:34:51 -0500

    commit - ProductoServiceImpl .

--- src/main/java/serviceImpl/ProductoServiceImpl.java
+++ src/main/java/serviceImpl/ProductoServiceImpl.java
@@ -1,6 +1,37 @@
 package serviceImpl;
 
+import model.Producto;
+import repository.ProductoRepository;
+import service.ProductoService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class ProductoServiceImpl {
+import java.util.List;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+    @Autowired
+    ProductoRepository repository;
+
+    @Override
+    public List<Producto> listar() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void guardar(Producto producto) {
+        repository.save(producto);
+    }
+
+    @Override
+    public Producto buscar(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        repository.deleteById(id);
+    }   
 }
