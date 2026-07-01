9fa46bddac9b3af791f480e0d4373571717a21b7
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-01 16:44:53 -0500

    commit - index / .

--- src/main/java/CrudProductosApplication.java
+++ src/main/java/CrudProductosApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudProductosApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CrudProductosApplication.class, args);
 	}
-
 }
