cb85910e1846b454ec2c92925b8df9421a536666
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-01 11:13:20 -0500

    commit - Producto .

--- src/main/java/model/Producto.java
+++ src/main/java/model/Producto.java
@@ -1,4 +1,38 @@
 package model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    public Producto() {
+
+    }
+
+    public Producto(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }   
 }
